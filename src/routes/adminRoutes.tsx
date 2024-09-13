8a83e8122cfb644cd26a98ad26a12de3f380558c
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-13 23:45:20 +0600

    create a route handle

---
+++ src/routes/adminRoutes.tsx
@@ -0,0 +1,43 @@
+import AdminDashboard from "../pages/Admin/AdminDashboard";
+import CreateAdmin from "../pages/Admin/CreateAdmin";
+import CreateFaculty from "../pages/Admin/CreateFaculty";
+import CreateStudent from "../pages/Admin/CreateStudent";
+
+export const adminPaths = [
+  {
+    path: "dashboard",
+    element: <AdminDashboard />,
+  },
+  {
+    path: "create-student",
+    element: <CreateStudent />,
+  },
+  {
+    path: "create-admin",
+    element: <CreateAdmin />,
+  },
+  {
+    path: "create-faculty",
+    element: <CreateFaculty />,
+  },
+];
+
+const newArray = adminPaths.reduce((acc, item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+
+  return acc;
+}, []);
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,10 +4,7 @@ import About from "../pages/About.js";
 import Contact from "../pages/Contact.js";
 import Login from "../pages/Login.js";
 import Register from "../pages/Register.js";
-import AdminDashboard from "../pages/Admin/AdminDashboard.js";
-import CreateStudent from "../pages/Admin/CreateStudent.js";
-import CreateAdmin from "../pages/Admin/CreateAdmin.js";
-import CreateFaculty from "../pages/Admin/CreateFaculty.js";
+import { adminPaths } from "./adminRoutes.js";
 
 const router = createBrowserRouter([
   {
@@ -27,24 +24,7 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: [
-      {
-        path: "dashboard",
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-      {
-        path: "create-admin",
-        element: <CreateAdmin />,
-      },
-      {
-        path: "create-faculty",
-        element: <CreateFaculty />,
-      },
-    ],
+    children: adminPaths,
   },
   {
     path: "/login",
