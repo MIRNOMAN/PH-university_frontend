4f02a669cdbc39d5479bb729e95bf38b873f3431
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-03 01:29:47 +0600

    create search query

--- src/pages/Admin/AcademicManagement/AcademicSemester.tsx
+++ src/pages/Admin/AcademicManagement/AcademicSemester.tsx
@@ -95,33 +95,6 @@ const AcademicSemester = () => {
     },
   ];
 
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-      address: "New York No. 1 Lake Park",
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-      address: "London No. 1 Lake Park",
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      age: 32,
-      address: "Sydney No. 1 Lake Park",
-    },
-    {
-      key: "4",
-      name: "Jim Red",
-      age: 32,
-      address: "London No. 2 Lake Park",
-    },
-  ];
-
   const onChange: TableProps<DataType>["onChange"] = (
     pagination,
     filters,
@@ -134,7 +107,7 @@ const AcademicSemester = () => {
   return (
     <Table<DataType>
       columns={columns}
-      dataSource={data}
+      dataSource={tableData}
       onChange={onChange}
       showSorterTooltip={{ target: "sorter-icon" }}
     />
