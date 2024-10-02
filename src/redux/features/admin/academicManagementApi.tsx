819cb84fd5868d1281367fcb9b732b661476c2e9
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-03 01:31:54 +0600

    academic semester api

--- src/redux/features/admin/academicManagementApi.tsx
+++ src/redux/features/admin/academicManagementApi.tsx
@@ -1,14 +1,26 @@
 import { TAcademicSemester } from "../../../types/AcademicManagement-type";
-import { TResponseRedux } from "../../../types/global";
+import { TQueryParam, TResponseRedux } from "../../../types/global";
 import { baseApi } from "../../api/baseApi";
 
 const academicManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getAllSemester: builder.query({
-      query: () => ({
-        url: "/academic-semesters",
-        method: "GET",
-      }),
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/academic-semesters",
+          method: "GET",
+          params: params,
+        };
+      },
       transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
         return {
           data: response.data,
