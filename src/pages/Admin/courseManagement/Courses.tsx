b0fa9c2a657d2872957188cdc4e8683bd03219eb
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-09 02:00:25 +0600

    update course data

--- src/pages/Admin/courseManagement/Courses.tsx
+++ src/pages/Admin/courseManagement/Courses.tsx
@@ -1,6 +1,34 @@
+import { useGetAllCoursesQuery } from "../../../redux/features/admin/courseManagementApi";
 
 
 const Courses = () => {
+    const { data: courses, isFetching } = useGetAllCoursesQuery(undefined);
+
+    const tableData = courses?.data?.map(({ _id, title, prefix, code }) => ({
+      key: _id,
+      title,
+      code: `${prefix}${code}`,
+    }));
+  
+    const columns = [
+      {
+        title: 'Title',
+        key: 'title',
+        dataIndex: 'title',
+      },
+      {
+        title: 'Code',
+        key: 'code',
+        dataIndex: 'code',
+      },
+      {
+        title: 'Action',
+        key: 'x',
+        render: (item) => {
+          return <AddFacultyModal facultyInfo={item} />;
+        },
+      },
+    ];
     return (
         <div>
              <h1>this is course</h1>
