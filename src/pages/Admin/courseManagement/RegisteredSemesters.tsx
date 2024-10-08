7b3ae628f94bb47ab00c4da898d8b54ffbda2540
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-08 21:25:53 +0600

    create Registered Semesters

---
+++ src/pages/Admin/courseManagement/RegisteredSemesters.tsx
@@ -0,0 +1,11 @@
+
+
+const RegisteredSemesters = () => {
+    return (
+        <div>
+             <h1>this is Registered Semesters</h1>
+        </div>
+    );
+};
+
+export default RegisteredSemesters;
