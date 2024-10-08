2b970b8b8a51bc67222c6dd6f688775cdcf47e98
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-08 22:23:18 +0600

    create registered semester data

--- src/pages/Admin/courseManagement/RegisteredSemesters.tsx
+++ src/pages/Admin/courseManagement/RegisteredSemesters.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import { useGetAllRegisteredSemestersQuery, useUpdateRegisteredSemesterMutation } from "../../../redux/features/admin/courseManagementApi";
 
+
+const items = [
+    {
+      label: 'Upcoming',
+      key: 'UPCOMING',
+    },
+    {
+      label: 'Ongoing',
+      key: 'ONGOING',
+    },
+    {
+      label: 'Ended',
+      key: 'ENDED',
+    },
+  ];
 
 const RegisteredSemesters = () => {
+    const [semesterId, setSemesterId] = useState('');
+    const { data: semesterData, isFetching } =
+      useGetAllRegisteredSemestersQuery(undefined);
+  
+    const [updateSemesterStatus] = useUpdateRegisteredSemesterMutation();
+  
+    console.log(semesterId);
+  
     return (
         <div>
              <h1>this is Registered Semesters</h1>
