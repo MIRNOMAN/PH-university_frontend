014c40350efbf99a43e07fc000c45cdbc47a067b
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-08 22:08:58 +0600

    semster registation data

--- src/pages/Admin/courseManagement/SemesterRegistration.tsx
+++ src/pages/Admin/courseManagement/SemesterRegistration.tsx
@@ -1,6 +1,15 @@
 
 
 const SemesterRegistration = () => {
+    const [addSemester] = useAddRegisteredSemesterMutation();
+    const { data: academicSemester } = useGetAllSemestersQuery([
+      { name: 'sort', value: 'year' },
+    ]);
+  
+    const academicSemesterOptions = academicSemester?.data?.map((item) => ({
+      value: item._id,
+      label: `${item.name} ${item.year}`,
+    }));
     return (
         <div>
                 <h1>this is Semester Registration</h1>
