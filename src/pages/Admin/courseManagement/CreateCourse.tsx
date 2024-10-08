629aa66309b39c7f66f3b83058667eaf463d4855
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-09 01:24:51 +0600

    create course

--- src/pages/Admin/courseManagement/CreateCourse.tsx
+++ src/pages/Admin/courseManagement/CreateCourse.tsx
@@ -1,5 +1,13 @@
+import { useAddCourseMutation, useGetAllCoursesQuery } from "../../../redux/features/admin/courseManagementApi";
 
 const CreateCourse = () => {
+    const [createCourse] = useAddCourseMutation();
+    const { data: courses } = useGetAllCoursesQuery(undefined);
+  
+    const preRequisiteCoursesOptions = courses?.data?.map((item) => ({
+      value: item._id,
+      label: item.title,
+    }));
     return (
         <div>
             <h1>this is create course</h1>
