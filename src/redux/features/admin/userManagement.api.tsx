8f9c58d3751c84d3354d376895f2c740d68cc99f
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-09 01:58:51 +0600

    update course api

--- src/redux/features/admin/userManagement.api.tsx
+++ src/redux/features/admin/userManagement.api.tsx
@@ -1,6 +1,8 @@
-import { TQueryParam, TResponseRedux } from "../../../types/global";
-import { TStudent } from "../../../types/userManagement.type";
-import { baseApi } from "../../api/baseApi";
+
+
+import { TQueryParam, TResponseRedux } from '../../../types/global';
+import { TStudent } from '../../../types/userManagement.type';
+import { baseApi } from '../../api/baseApi';
 
 const userManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -16,8 +18,32 @@ const userManagementApi = baseApi.injectEndpoints({
         }
 
         return {
-          url: "/students",
-          method: "GET",
+          url: '/students',
+          method: 'GET',
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TStudent[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    getAllFaculties: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: '/faculties',
+          method: 'GET',
           params: params,
         };
       },
@@ -30,13 +56,16 @@ const userManagementApi = baseApi.injectEndpoints({
     }),
     addStudent: builder.mutation({
       query: (data) => ({
-        url: "/users/create-student",
-        method: "POST",
+        url: '/users/create-student',
+        method: 'POST',
         body: data,
       }),
     }),
   }),
 });
 
-export const { useAddStudentMutation, useGetAllStudentsQuery } =
-  userManagementApi;
+export const {
+  useAddStudentMutation,
+  useGetAllStudentsQuery,
+  useGetAllFacultiesQuery,
+} = userManagementApi;
