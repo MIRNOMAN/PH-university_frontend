2d85f27fb38a25068c49a136ca9bf0e0f8e50b7e
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-08 13:27:32 +0600

    create data

--- src/pages/Admin/userManagement/StudentData.tsx
+++ src/pages/Admin/userManagement/StudentData.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { TStudent } from "../../../types/userManagement.type";
+import { TQueryParam } from "../../../types/global";
+import { useGetAllStudentsQuery } from "../../../redux/features/admin/userManagement.api";
 
 export type TTableData = Pick<
   TStudent,
@@ -6,6 +9,19 @@ export type TTableData = Pick<
 >;  
 
 const StudentData = () => {
+    const [params, setParams] = useState<TQueryParam[]>([]);
+  const [page, setPage] = useState(1);
+  const {
+    data: studentData,
+    isLoading,
+    isFetching,
+  } = useGetAllStudentsQuery([
+    { name: 'page', value: page },
+    { name: 'sort', value: 'id' },
+    ...params,
+  ]);
+
+  console.log({ isLoading, isFetching });
     return (
         <div>
             <h1>stdent data</h1>
