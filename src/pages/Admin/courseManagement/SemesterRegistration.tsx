0b81a9a3839a8db983a7e7e744c464ba76c95966
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-08 21:26:53 +0600

    create Semester Registration

---
+++ src/pages/Admin/courseManagement/SemesterRegistration.tsx
@@ -0,0 +1,11 @@
+
+
+const SemesterRegistration = () => {
+    return (
+        <div>
+                <h1>this is Semester Registration</h1>
+        </div>
+    );
+};
+
+export default SemesterRegistration;
