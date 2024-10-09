28695be45119164324233a5af5cda541a057a72b
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-10 00:49:03 +0600

    create ph select watch form

--- src/components/form/PHSelectWithWatch.tsx
+++ src/components/form/PHSelectWithWatch.tsx
@@ -28,3 +28,26 @@ const PHSelectWithWatch = ({
   useEffect(() => {
     onValueChange(inputValue);
   }, [inputValue]);
+
+
+  return (
+    <Controller
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <Form.Item label={label}>
+          <Select
+            mode={mode}
+            style={{ width: '100%' }}
+            {...field}
+            options={options}
+            size="large"
+            disabled={disabled}
+          />
+          {error && <small style={{ color: 'red' }}>{error.message}</small>}
+        </Form.Item>
+      )}
+    />
+  );
+};
+
+export default PHSelectWithWatch;
