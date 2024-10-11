3a9065f41a637d2d4f012acf88ceac42bcb5d8b3
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-11 20:50:20 +0600

    create PHTimePicker form

--- src/components/form/PHTimePicker.tsx
+++ src/components/form/PHTimePicker.tsx
@@ -8,3 +8,28 @@ type TPHDatePicker = {
 
 const PHTimePicker = ({ name, label }: TPHDatePicker) => {
   const { control } = useFormContext();
+
+  return (
+    <div style={{ marginBottom: '10px' }}>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field, fieldState: { error } }) => (
+          <>
+            <Form.Item label={label}>
+              <TimePicker
+                {...field}
+                size="large"
+                style={{ width: '100%' }}
+                format="HH:mm"
+              />
+              {error && <small style={{ color: 'red' }}>{error.message}</small>}
+            </Form.Item>
+          </>
+        )}
+      ></Controller>
+    </div>
+  );
+};
+
+export default PHTimePicker;
