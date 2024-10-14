5db588caab67c2ccff67868168d75f997d93f58b
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-14 23:26:46 +0600

    create state

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -1,10 +1,25 @@
+import { Button, Row } from "antd";
+import PHForm from "../components/form/PHForm";
+import PHInput from "../components/form/PHInput";
+import { useChangePasswordMutation } from "../redux/features/admin/userManagement.api";
+import { useAppDispatch } from "../redux/hooks";
+import { useNavigate } from "react-router-dom";
 
 
 const ChangePassword = () => {
+    const [changePassword] = useChangePasswordMutation();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+
     return (
-        <div>
-            
-        </div>
+        <Row justify="center" align="middle" style={{ height: '100vh' }}>
+      <PHForm onSubmit={onSubmit}>
+        <PHInput type="text" name="oldPassword" label="Old Password" />
+        <PHInput type="text" name="newPassword" label="New Password" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
     );
 };
 
