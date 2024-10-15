3238b4284292e758d1a4152bd99160c2cc514355
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-15 23:05:24 +0600

    single offered course

--- src/pages/Student/StudentOfferedCourse.tsx
+++ src/pages/Student/StudentOfferedCourse.tsx
@@ -1,9 +1,26 @@
+import { useEnrolCourseMutation, useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement.api";
 
 type TCourse = {
   [index: string]: any;
 };
 
 const StudentOfferedCourse = () => {
+  const { data: offeredCourseData } = useGetAllOfferedCoursesQuery(undefined);
+  const [enroll] = useEnrolCourseMutation();
+
+  const singleObject = offeredCourseData?.data?.reduce((acc: TCourse, item) => {
+    const key = item.course.title;
+    acc[key] = acc[key] || { courseTitle: key, sections: [] };
+    acc[key].sections.push({
+      section: item.section,
+      _id: item._id,
+      days: item.days,
+      startTime: item.startTime,
+      endTime: item.endTime,
+    });
+    return acc;
+  }, {});
+
   return (
     <div>
       <h1>This is student offered course</h1>
