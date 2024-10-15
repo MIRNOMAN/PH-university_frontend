4cb7dba1905f64108be1f2dba1bf45f9f225c1c6
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-16 00:07:49 +0600

    create faculty coursees

--- src/pages/Faculty/MyCourses.tsx
+++ src/pages/Faculty/MyCourses.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
 
 
 export const MyCourses = () => {
+    const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery(undefined);
+    const navigate = useNavigate();
+  
+    console.log(facultyCoursesData);
   return (
     <div>MyCourses</div>
   )
