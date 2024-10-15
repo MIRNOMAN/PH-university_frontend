78c56221d0221aca10290e5110828debfb88bb53
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-16 00:09:09 +0600

    create faculty coursee option semester option

--- src/pages/Faculty/MyCourses.tsx
+++ src/pages/Faculty/MyCourses.tsx
@@ -5,6 +5,17 @@ import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facul
 export const MyCourses = () => {
     const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery(undefined);
     const navigate = useNavigate();
+
+    const semesterOptions = facultyCoursesData?.data?.map((item) => ({
+        label: `${item.academicSemester.name} ${item.academicSemester.year}`,
+        value: item.semesterRegistration._id,
+      }));
+    
+      const courseOptions = facultyCoursesData?.data?.map((item) => ({
+        label: item.course.title,
+        value: item.course._id,
+      }));
+    
   
     console.log(facultyCoursesData);
   return (
