9e5f9b076e9786ba230a92f9ee0a276b1667c4a6
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-15 23:38:12 +0600

    create my schedual data

--- src/pages/Student/MySchedule.tsx
+++ src/pages/Student/MySchedule.tsx
@@ -5,6 +5,20 @@ export const MySchedule = () => {
     const { data } = useGetAllEnrolledCoursesQuery(undefined);
   console.log(data);
   return (
-    <div>MySchedule</div>
+    <div>
+    {data?.data?.map((item) => {
+      return (
+        <div>
+          <div>{item.course.title}</div>
+          <div>{item.offeredCourse.section}</div>
+          <div>
+            {item.offeredCourse.days.map((item) => (
+              <span> {item}</span>
+            ))}
+          </div>
+        </div>
+      );
+    })}
+  </div>
   )
 }
