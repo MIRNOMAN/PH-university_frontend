642d4bd35a54bc7556cf062c92134c86f4caad2b
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-16 16:19:11 +0600

    create faculty course data

--- src/pages/Faculty/MyStudents.tsx
+++ src/pages/Faculty/MyStudents.tsx
@@ -7,6 +7,21 @@ export const MyStudents = () => {
   const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery([
     { name: 'semesterRegistration', value: registerSemesterId },
     { name: 'course', value: courseId },
+
+
+    console.log(facultyCoursesData);
+
+    const tableData = facultyCoursesData?.data?.map(
+      ({ _id, student, semesterRegistration, offeredCourse }) => ({
+        key: _id,
+        name: student.fullName,
+        roll: student.id,
+        semesterRegistration: semesterRegistration._id,
+        student: student._id,
+        offeredCourse: offeredCourse._id,
+      })
+    );
+  
   ]);
   return (
     <div>MyStudents</div>
