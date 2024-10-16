6c30be9b1379f97a7b9377c44beb8838862bf139
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-10-16 16:18:22 +0600

    create mystudent data

--- src/pages/Faculty/MyStudents.tsx
+++ src/pages/Faculty/MyStudents.tsx
@@ -1,6 +1,13 @@
+import { useParams } from "react-router-dom";
+import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
 
 
 export const MyStudents = () => {
+  const { registerSemesterId, courseId } = useParams();
+  const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery([
+    { name: 'semesterRegistration', value: registerSemesterId },
+    { name: 'course', value: courseId },
+  ]);
   return (
     <div>MyStudents</div>
   )
